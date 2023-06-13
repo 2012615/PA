a219f0118d699f7a725bfd8d6ba91ade79aaef3d
tracer-ics2017 <tracer@njuics.org>
2023-06-13 10:47:51 +0800

    >  compile 2012615 zjs Linux debian 4.19.0-24-686-pae #1 SMP Debian 4.19.282-1 (2023-04-29) i686 GNU/Linux  10:47:51 up  3:32,  1 user,  load average: 0.09, 0.06, 0.04 8fda0b6dbab2d8e8342e3b59184657898c3b6fbd

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -10,24 +10,35 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
     CC_S, CC_NS, CC_P,  CC_NP,
     CC_L, CC_NL, CC_LE, CC_NLE
   };
-
   // TODO: Query EFLAGS to determine whether the condition code is satisfied.
   // dest <- ( cc is satisfied ? 1 : 0)
   switch (subcode & 0xe) {
     case CC_O:
+      rtl_get_OF(dest);
+      break;
     case CC_B:
+      rtl_get_CF(dest);
+      break;
     case CC_E:
+      rtl_get_ZF(dest);
+      break;
+    case CC_NE:
+      rtl_get_ZF(dest);
+      rtl_eq0(dest, dest);
+      break;
     case CC_BE:
+      rtl_get_CF(&t0);
+      rtl_get_ZF(dest);
+      rtl_or(dest,dest,&t0);
+      break;
     case CC_S:
-    case CC_L:
-    case CC_LE:
-      TODO();
+      rtl_get_SF(dest);
+      break;
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
-  }
+}
 
   if (invert) {
     rtl_xori(dest, dest, 0x1);
   }
 }
-
