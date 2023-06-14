4700c51b3e848089535acab37b6a0bb9a3661b1c
tracer-ics2017 <tracer@njuics.org>
2023-06-14 15:44:15 +0800

    >  run 2012615 zjs Linux debian 4.19.0-24-686-pae #1 SMP Debian 4.19.282-1 (2023-04-29) i686 GNU/Linux  15:44:15 up 18:58,  1 user,  load average: 0.26, 0.11, 0.14 9bb2f56e440b208ca2e992f0811497329e7497

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -2,7 +2,7 @@
 
 /* Uncomment these macros to enable corresponding functionality. */
 #define HAS_ASYE
-//#define HAS_PTE
+#define HAS_PTE
 
 void init_mm(void);
 void init_ramdisk(void);
@@ -28,8 +28,15 @@ int main() {
   init_irq();
 #endif
   init_fs();
-  uint32_t entry = loader(NULL, "/bin/bmptest");
-  ((void (*)(void))entry)();
+  //uint32_t entry = loader(NULL, "/bin/events");
+  //((void (*)(void))entry)();
+  extern void load_prog(const char* filename);
+  //execute mutiple process
+  load_prog("/bin/dummy");
+  //load_prog("/bin/hello");
+
+  _trap();
 
   panic("Should not reach here");
 }
+
