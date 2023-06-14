676b24ae145e4ccba8adc2bf453a70c1ad063985
tracer-ics2017 <tracer@njuics.org>
2023-06-14 21:33:17 +0800

    >  run 2012615 zjs Linux debian 4.19.0-24-686-pae #1 SMP Debian 4.19.282-1 (2023-04-29) i686 GNU/Linux  21:33:17 up 23:02,  1 user,  load average: 1.75, 1.15, 0.96 3ecaa1fcfe41864639d27fc871fa015b0b6d71

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -32,7 +32,7 @@ int main() {
   //((void (*)(void))entry)();
   extern void load_prog(const char* filename);
   //execute mutiple process
-  //load_prog("/bin/pal");
+  load_prog("/bin/pal");
   load_prog("/bin/hello");
 
 
